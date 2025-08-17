f4793bcdb7f6f571946685bdc3498d66f6574a72
thatimahe <thatimahe@gmail.com>
2025-08-17 20:49:05 +1000

    modified:   tests/home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,9 +1,10 @@
 import {test, expect} from '@playwright/test';
 test('Verify the URL and the logo', async ({ page }) => {
     await page.goto('https://auspost.com.au/');
+    // Check if the URL contains 'post'
     expect(page.url()).toContain('post');
+    // Check if the logo is visible
     await expect(page.locator('#globallogo-480318-706350')).toBeVisible();
-    //await page.getByAltText('cwc-w-25-logo').click();
-    //await expect(page.getByAltText('cwc-w-25-logo')).toBeVisible();
+    
     
 })
