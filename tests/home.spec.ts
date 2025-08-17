07ea9843bb120e80bf68e2dbac0b4916794e831b
thatimahe <thatimahe@gmail.com>
2025-08-17 22:39:08 +1000

    modified:   tests/home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -5,6 +5,15 @@ test('Verify the URL and the logo', async ({ page }) => {
     expect(page.url()).toContain('post');
     // Check if the logo is visible
     await expect(page.locator('#globallogo-480318-706350')).toBeVisible();
-    
-    
+    // Find the Search button and click on Search
+    const iconByRole = page.getByRole('button', { name: 'search' });
+    await iconByRole.click();
+    //Fill the search input with 'India'
+    await page.getByRole('combobox').fill('India');
+    // Click the search button 
+    //Search is not unique, so using a more specific selector
+    await page.locator('.search-input-form').getByRole('button', { name: 'Search' }).click();
+    // Verify that the search results are displayed
+    await expect (page.locator('text=No Results found')).toBeVisible();
+
 })
